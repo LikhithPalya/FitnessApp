762fecc9857644c47765bdd40d9107db20b906ae
personalPalya <likhithpalya@gmail.com>
2026-02-14 12:34:20 +0530

    add get user recommendation api

--- src/main/java/com/project/fitness/controller/RecommendationController.java
+++ src/main/java/com/project/fitness/controller/RecommendationController.java
@@ -6,18 +6,17 @@ import com.project.fitness.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/recommendation")
 @RequiredArgsConstructor
 public class RecommendationController {
 
-    @Autowired
     private final RecommendationService recommendationService;
+
     @PostMapping("/generate")
     public ResponseEntity<Recommendation> generateRecommendation(
             @RequestBody RecommendationRequest request
@@ -25,4 +24,15 @@ public class RecommendationController {
         Recommendation recommendation = recommendationService.generaterecommendations(request);
         return ResponseEntity.ok(recommendation);
     }
+
+    @GetMapping("/user/{userid}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(
+            @PathVariable("userid") String userId //accepitng a dynamic variable via the path
+    ){
+        List<Recommendation> recommendationList = recommendationService.getUserRecommendation(userId); //a single user can have multiple recommendations
+        return ResponseEntity.ok(recommendationList);
+    }
+
+
+
 }
--- src/main/java/com/project/fitness/service/RecommendationService.java
+++ src/main/java/com/project/fitness/service/RecommendationService.java
@@ -10,6 +10,8 @@ import com.project.fitness.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
@@ -36,5 +38,9 @@ public class RecommendationService {
                 .build();
 
         return recommendationRepository.save(recommendation);
+    }
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId); // we dont want recommendation i.e id iinstead we want user mapped with recommendation
     }
 }
